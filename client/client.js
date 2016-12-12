000cbcd43b48df21623a9f89698e31b7ea8725ad
Andrej Mihajlov <and@codeispoetry.ru>
2016-12-12 15:11:49 +0000

    Add REQ/REP socket

--- client/client.js
+++ client/client.js
@@ -1,8 +1,19 @@
 var zmq = require('zmq');
-var socket = zmq.socket('pull');
+var pull_socket = zmq.socket('pull');
+var req_socket = zmq.socket('req');
 
-socket.on('message', function (message) {
+pull_socket.on('message', function (message) {
   console.log('Received: ' + message.toString('utf8'));
 });
 
-socket.connect('tcp://127.0.0.1:9998');
+req_socket.on('message', function (message) {
+  console.log('<- ' + message.toString('utf8'));
+});
+
+setInterval(function () {
+  req_socket.send("Ping");
+  console.log("-> Ping");
+}, 1000);
+
+pull_socket.connect('tcp://127.0.0.1:9998');
+req_socket.connect('tcp://127.0.0.1:9999');
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,43 @@
 extern crate zmq;
 
-use std::thread::sleep;
-use std::time::Duration;
+use std::thread;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 fn main() {
     println!("ZeroMQ server.");
 
     let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::PUSH).unwrap();
+    let push_socket = ctx.socket(zmq::PUSH).unwrap();
+    push_socket.bind("tcp://127.0.0.1:9998").unwrap();
 
-    socket.bind("tcp://127.0.0.1:9998").unwrap();
+    let rep_socket = ctx.socket(zmq::REP).unwrap();
+    rep_socket.bind("tcp://127.0.0.1:9999").unwrap();
 
-    loop {
-        socket.send_str("Hello world!", 0).unwrap();
-        println!("Sent data");
-        sleep(Duration::from_secs(1))
-    }
+    let push_thread = thread::spawn(move || {
+        loop {
+            let time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+            let msg = format!("Time is {}", time);
+
+            push_socket.send_str(&msg, 0).unwrap();
+
+            println!("Sent data");
+
+            thread::sleep(Duration::from_secs(3));
+        }
+    });
+
+    let rep_thread = thread::spawn(move || {
+        let mut msg = zmq::Message::new().unwrap();
+
+        loop {
+            rep_socket.recv(&mut msg, 0).unwrap();
+
+            println!("Received {}", msg.as_str().unwrap());
+
+            rep_socket.send_str("Pong", 0).unwrap();
+        }
+    });
+
+    push_thread.join();
+    rep_thread.join();
 }
